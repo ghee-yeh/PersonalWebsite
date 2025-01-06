c8b209e9cc33240d57c4c3bb8e6c43143d68777f
Guillermo Avelar <guillermo@avelar.family>
2025-01-06 13:45:52 -0500

    Traveler link

--- app/experience/page.js
+++ app/experience/page.js
@@ -13,45 +13,109 @@ import ftsl from "../../public/ftsl.jpeg";
 import translation from "../_internationalization/translation.json";
 
 export default function Experience() {
+  const [isVisible, setIsVisible] = useState({});
+  const [language, setLanguage] = useState("en");
 
-    const [isVisible, setIsVisible] = useState({});
-    const [language, setLanguage] = useState("en");
+  useEffect(() => {
+    if (window) {
+      const savedLanguage = localStorage.getItem("language") || "en";
+      setLanguage(savedLanguage);
+    }
 
-    useEffect(() => {
-        if (window) {
-            const savedLanguage = localStorage.getItem("language") || "en";
-            setLanguage(savedLanguage);
-        }
+    const elements = 10;
+    for (let i = 0; i < elements; i++) {
+      setTimeout(() => {
+        setIsVisible((prev) => ({ ...prev, [i]: true }));
+      }, i * 100);
+    }
+  }, []);
 
-        const elements = 10;
-        for (let i = 0; i < elements; i++) {
-          setTimeout(() => {
-            setIsVisible(prev => ({...prev, [i]: true}));
-          }, i * 100);
-        }
-      }, []);
-    
-      const fadeInStyle = (index) => ({
-        opacity: isVisible[index] ? 1 : 0,
-        transition: 'opacity 0.5s ease-in',
-        color: "var(--primary)"
-      });
+  const fadeInStyle = (index) => ({
+    opacity: isVisible[index] ? 1 : 0,
+    transition: "opacity 0.5s ease-in",
+    color: "var(--primary)",
+  });
 
-  return <div className={styles.container} style={{ minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
-    <div style={fadeInStyle(0)}><NavBar /></div>
-    <div style={{ flex: 1, gap: "40px", display: "flex", flexDirection: "column" }}>
-        <div style={{display: "flex", flexDirection: "column", gap: "20px"}}>
-      <div className="b1" style={{...fadeInStyle(1), fontWeight: "bold"}}>{translation[language].experience}</div>
-      <div style={fadeInStyle(2)}><ExperienceCard company="Habitat App" position={translation[language].founder} description={translation[language].habitat_app} year={`2022 - ${translation[language].present}`} image={habitat} url="https://www.habitat-app.org"/></div>
-      <div style={fadeInStyle(3)}><ExperienceCard company="traveler" position={translation[language].co_founder} description={translation[language].traveler} year="2024" image={traveler} url="https://www.habitat-app.org"/></div>
-      <div style={fadeInStyle(3)}><ExperienceCard company="Picnic" position="Chief Product Officer" description={translation[language].picnic} year="2021" image={picnic} url="https://apps.apple.com/ca/app/picnic-dive-into-communities/id1544626265"/></div>
+  return (
+    <div
+      className={styles.container}
+      style={{ minHeight: "100vh", display: "flex", flexDirection: "column" }}
+    >
+      <div style={fadeInStyle(0)}>
+        <NavBar />
       </div>
-      <div style={{display: "flex", flexDirection: "column", gap: "20px"}}>
-      <div className="b1" style={{...fadeInStyle(4), fontWeight: "bold"}}>{translation[language].volunteering}</div>
-      <div style={fadeInStyle(5)}><ExperienceCard company="FTSL" position={translation[language].founder} description={translation[language].ftsl} year="2018 - 2024" image={ftsl} url="https://www.instagram.com/fundacionftsl/"/></div>
-      <div style={fadeInStyle(6)}><ExperienceCard company="SivarLive" position={translation[language].co_founder} description={translation[language].sivar} year="2022" image={sivar} url="https://www.instagram.com/sivar.live/"/></div>
+      <div
+        style={{
+          flex: 1,
+          gap: "40px",
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <div style={{ display: "flex", flexDirection: "column", gap: "20px" }}>
+          <div className="b1" style={{ ...fadeInStyle(1), fontWeight: "bold" }}>
+            {translation[language].experience}
+          </div>
+          <div style={fadeInStyle(2)}>
+            <ExperienceCard
+              company="Habitat App"
+              position={translation[language].founder}
+              description={translation[language].habitat_app}
+              year={`2022 - ${translation[language].present}`}
+              image={habitat}
+              url="https://www.habitat-app.org"
+            />
+          </div>
+          <div style={fadeInStyle(3)}>
+            <ExperienceCard
+              company="traveler"
+              position={translation[language].co_founder}
+              description={translation[language].traveler}
+              year="2024"
+              image={traveler}
+              url="https://www.get-traveler.com/"
+            />
+          </div>
+          <div style={fadeInStyle(3)}>
+            <ExperienceCard
+              company="Picnic"
+              position="Chief Product Officer"
+              description={translation[language].picnic}
+              year="2021"
+              image={picnic}
+              url="https://apps.apple.com/ca/app/picnic-dive-into-communities/id1544626265"
+            />
+          </div>
+        </div>
+        <div style={{ display: "flex", flexDirection: "column", gap: "20px" }}>
+          <div className="b1" style={{ ...fadeInStyle(4), fontWeight: "bold" }}>
+            {translation[language].volunteering}
+          </div>
+          <div style={fadeInStyle(5)}>
+            <ExperienceCard
+              company="FTSL"
+              position={translation[language].founder}
+              description={translation[language].ftsl}
+              year="2018 - 2024"
+              image={ftsl}
+              url="https://www.instagram.com/fundacionftsl/"
+            />
+          </div>
+          <div style={fadeInStyle(6)}>
+            <ExperienceCard
+              company="SivarLive"
+              position={translation[language].co_founder}
+              description={translation[language].sivar}
+              year="2022"
+              image={sivar}
+              url="https://www.instagram.com/sivar.live/"
+            />
+          </div>
+        </div>
+      </div>
+      <div style={fadeInStyle(4)}>
+        <Footer />
+      </div>
     </div>
-    </div>
-    <div style={fadeInStyle(4)}><Footer /></div>
-  </div>;
+  );
 }
