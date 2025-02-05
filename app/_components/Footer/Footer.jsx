49c93f7a35bb2c1f28463614f5cf89ae109eaa38
Guillermo Avelar <guillermo@avelar.family>
2025-02-05 18:37:40 -0500

    fix email

--- app/_components/Footer/Footer.jsx
+++ app/_components/Footer/Footer.jsx
@@ -1,23 +1,33 @@
 "use client";
 
-import styles from './style.module.css';
-import translation from '../../_internationalization/translation.json';
-import { useEffect, useState } from 'react';
+import styles from "./style.module.css";
+import translation from "../../_internationalization/translation.json";
+import { useEffect, useState } from "react";
 export default function Footer() {
+  const [language, setLanguage] = useState("en");
 
-    const [language, setLanguage] = useState("en");
+  useEffect(() => {
+    if (window) {
+      const savedLanguage = localStorage.getItem("language") || "en";
+      setLanguage(savedLanguage);
+    }
+  }, []);
 
-    useEffect(() => {
-        if (window) {
-            const savedLanguage = localStorage.getItem("language") || "en";
-            setLanguage(savedLanguage);
-        }
-    }, []);
-
-return <div className={styles.links}>
-            <a className="b3" href="https://www.x.com/gee_yer_moh/" target="_blank">@gee_yer_moh</a>
-            <a className="b3" href="https://github.com/gee-yer-moh" target="_blank">github</a>
-            <a className="b3" href="https://www.linkedin.com/in/jose-guillermo-avelar/" target="_blank">linkedin</a>
-            <a className="b3" href="mailto:jga2131@columbia.edu" target="_blank">{translation[language].email}</a>
-    </div>;
+  return (
+    <div className={styles.links}>
+      <a className="b3" href="https://www.x.com/gee_yer_moh/" target="_blank">
+        @gee_yer_moh
+      </a>
+      <a className="b3" href="https://github.com/gee-yer-moh" target="_blank">
+        github
+      </a>
+      <a
+        className="b3"
+        href="https://www.linkedin.com/in/jose-guillermo-avelar/"
+        target="_blank"
+      >
+        linkedin
+      </a>
+    </div>
+  );
 }
